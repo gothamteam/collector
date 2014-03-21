9486a072a6a3c805e73be1bc04ec94f2043cdb7e
wangsuse <wangsuse2011@gmail.com>
2014-03-20 23:59:49 -0400

    leaning

--- YelpCollector/src/scrapy/tutorial/tutorial/items.py
+++ YelpCollector/src/scrapy/tutorial/tutorial/items.py
@@ -5,7 +5,9 @@
 
 from scrapy.item import Item, Field
 
-class TutorialItem(Item):
+class DmozItem(Item):
     # define the fields for your item here like:
     # name = Field()
-    pass
+    title = Field()
+    link = Field()
+    desc = Field()
